8e2356de4d4bc6a4546e8f117dd81bba7fcc5e61
Ta Quang Chien <chien1472k4@gmail.com>
2025-03-06 21:04:57 +0700

    first commit

---
+++ Student.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaiTapTuan3_Bai1
+{
+    class Student : PerSon
+
+    {
+        public string masv;
+        public double dtb;
+        public string email;
+
+        public Student() { }
+        public Student(string hoten, string gioitinh, string diachi, int namsinh,string masv, double dtb, string email)
+            : base (hoten, gioitinh, diachi, namsinh)
+        {
+            this.masv = masv;
+            this.dtb = dtb;
+            this.email = email;
+        }
+        public string Email
+        {
+            get { return email; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value) || !value.Contains("@"))
+                {
+                    throw new ArgumentException("Email không hợp lệ! Phải chứa ký tự '@'.");
+                }
+                email = value;
+            }
+        }
+        public override void inputInfo()
+        {
+            base.inputInfo();
+            Console.Write("Nhap ma sinh vien : ");
+            masv = Console.ReadLine();
+            Console.Write("Nhap diem trung binh : ");
+            dtb = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Nhap email : ");
+            email = Console.ReadLine();
+        }
+        public override void showInfo()
+        {
+            base.showInfo();
+            Console.WriteLine("MA SINH VIEN : " + masv);
+            Console.WriteLine("DIEM TRUNG BINH : " + dtb);
+            Console.WriteLine("EMAIL : " + email);
+        }
+        public double GetDTB()
+        {
+            return dtb;
+        }
+        public string GetMASV()
+        {
+            return masv;
+        }
+        public bool HocBong()
+        {
+            return dtb >= 8;
+        }
+    }
+}
---
+++ perSon.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+ namespace BaiTapTuan3_Bai1
+{
+    public class PerSon
+    {
+        public string hoten, gioitinh, diachi;
+        public int namsinh;
+        public int stt;
+
+        public PerSon() { }
+        public PerSon (string hoten, string gioitinh, string diachi, int namsinh)
+        {
+            this.hoten = hoten;
+            this.gioitinh = gioitinh;
+            this.diachi = diachi;
+            this.namsinh = namsinh;
+        }
+        public virtual void inputInfo()
+        {
+            Console.Write("Nhap ho va ten : ");
+            hoten = Console.ReadLine();
+            Console.Write("Nhap gioi tinh : ");
+            gioitinh = Console.ReadLine();
+            Console.Write("Nhap dia chi : ");
+            diachi  = Console.ReadLine();
+            Console.Write("Nhap nam sinh : ");
+            namsinh = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Nhap stt : ");
+            stt = Convert.ToInt32(Console.ReadLine());
+        }
+        public virtual void showInfo()
+        {
+            Console.WriteLine("HO TEN : " + hoten);
+            Console.WriteLine("GIOI TINH : " + gioitinh);
+            Console.WriteLine("DIA CHI : " + diachi);
+            Console.WriteLine("NAM SINH : " + namsinh);
+        }
+
+    }
+    
+}
